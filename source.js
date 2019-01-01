d52ebe5a2213dd230453543b9311185c212c2799
lestarik <46297471+lestarik@users.noreply.github.com>
2019-01-01 20:37:36 +0700

    web pesona

---
+++ source.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var konfirmasi = confirm (" Apakah anda ingin Membuka Halaman ini ? ");
+ if (konfirmasi == true)
+ {
+  document.location.href="home.html"; 
+ }
+ else
+ {
+     document.location.href="home.html";
+ </script>
